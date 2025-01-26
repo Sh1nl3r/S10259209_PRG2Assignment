987d056fea486ed17e6d570ab297138093a94c49
Yongze <yongze1997@gmail.com>
2025-01-26 16:29:00 +0800

    NORMFlight class v1

---
+++ S10259209_PRG2Assignment/NORMFlight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10259209_PRG2Assignment
+{
+    internal class NORMFlight : Flight
+    {
+        public NORMFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+            : base(flightNumber, origin, destination, expectedTime, status)
+        {
+        }
+        public override double CalculateFees()
+        {
+            if (Destination == "Singapore (SIN)")
+            {
+                return 500.00;
+            }
+            else
+            {
+                return 800.00;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Fees: ${CalculateFees()}";
+        }
+    }
+}
