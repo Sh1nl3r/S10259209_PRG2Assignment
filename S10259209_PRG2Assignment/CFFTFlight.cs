08212c1e8eeca1d51a52a8d6177808dc22481eb4
Yongze <yongze1997@gmail.com>
2025-01-26 16:31:38 +0800

    CFFTFlight class v1

---
+++ S10259209_PRG2Assignment/CFFTFlight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10259209_PRG2Assignment
+{
+    internal class CFFTFlight : Flight
+    {
+        public double RequestFee { get; set; }
+
+        public CFFTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status, double requestFee)
+            : base(flightNumber, origin, destination, expectedTime, status)
+        {
+            RequestFee = requestFee;
+        }
+
+        public override double CalculateFees()
+        {
+            if (Destination == "Singapore (SIN)")
+            {
+                return 500.00 + RequestFee;
+            }
+            else
+            {
+                return 800.00 + RequestFee;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Fees: {CalculateFees()}";
+        }
+    }
+
+}
