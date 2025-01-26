2f24ae07bf1cfd9cdcaed3c28573f18741a0d333
Yongze <yongze1997@gmail.com>
2025-01-26 16:30:50 +0800

    DDJBFlight class v1

---
+++ S10259209_PRG2Assignment/DDJBFlight.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10259209_PRG2Assignment
+{
+    internal class DDJBFlight : Flight
+    {
+        public double RequestFee { get; set; }
+
+        public DDJBFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status, double requestFee)
+            : base(flightNumber, origin, destination, expectedTime, status)
+        {
+            RequestFee = requestFee;
+        }
+        public override double CalculateFees()
+        {
+            if (Destination == "Singapore (SIN)")
+            {
+                return 500.00 + RequestFee;
+            }
+            else
+            {
+                return 800.00 + RequestFee;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Fees: {CalculateFees()}";
+        }
+    }
+}
