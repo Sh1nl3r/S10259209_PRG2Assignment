1b8dbb5059428d2f564049ae1eefc0eb6881df4e
Shin Ler <S10259209@connect.np.edu.sg>
2025-01-26 18:19:55 +0800

    Terminal class v1

---
+++ S10259209_PRG2Assignment/Terminal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10259209_PRG2Assignment
+{
+    internal class Terminal
+    {
+        public string TerminalName { get; set; }
+        public Dictionary<string, Airline> Airlines { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+        public Dictionary<string, double> GateFees { get; set; }
+
+        public Terminal() { }
+        public Terminal(string terminalName)
+        {
+            this.TerminalName = terminalName;
+            Airlines = new Dictionary<string, Airline>();
+            Flights = new Dictionary<string, Flight>();
+            BoardingGates = new Dictionary<string, BoardingGate>();
+            GateFees = new Dictionary<string, double>();
+        }
+
+        public bool AddAirline(Airline airline)
+        {
+            if (Airlines.ContainsKey(airline.Name))
+            {
+                return false;
+            }
+            else
+            {
+                Airlines.Add(airline.Name, airline);
+                return true;
+            }
+        }
+        public bool AddBoardingGate(BoardingGate boardingGate)
+        {
+            if (BoardingGates.ContainsKey(boardingGate.GateName))
+            {
+                return false;
+            }
+            else
+            {
+                BoardingGates.Add(boardingGate.GateName, boardingGate);
+                return true;
+            }
+        }
+        public Airline GetAirlineFromFlight(Flight flight)
+        {
+            foreach (var airline in Airlines.Values)
+            {
+                if (airline.Flights.ContainsKey(flight.FlightNumber))
+                {
+                    return airline;
+                }
+            }
+            return null;
+        }
+        public void PrintAirlineFees()
+        {
+            foreach (var airline in Airlines.Values)
+            {
+                Console.WriteLine($"Airline Fees: {airline.CalculateFees():F2}");
+            }
+        }
+        public override string ToString()
+        {
+            return TerminalName;
+        }
+    }
+}
