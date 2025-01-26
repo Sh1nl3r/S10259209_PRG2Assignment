7c71bf8177b36ac0f72dbead8cc075a9469060cc
Yongze <yongze1997@gmail.com>
2025-01-26 16:27:49 +0800

    Flight class v1

---
+++ S10259209_PRG2Assignment/Flight.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10259209_PRG2Assignment
+{
+    internal class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
+
+        public Flight() { }
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        {
+            this.FlightNumber = flightNumber;
+            this.Origin = origin;
+            this.Destination = destination;
+            this.ExpectedTime = expectedTime;
+            if (status == "")
+            {
+                this.Status = "None";
+            }
+            else
+            {
+                this.Status = status;
+            }
+
+        }
+
+        public virtual double CalculateFees()
+        {
+            return 0;
+        }
+        public override string ToString()
+        {
+            return $"Flight Number: {FlightNumber}, Origin: {Origin}, Destination: {Destination}, Expected Time: {ExpectedTime}, Status: {Status}";
+        }
+    }
+}
